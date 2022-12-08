087ae3f50a89db7704efc8aed3d2c22783506357
Iwona Behnke <behnke.iwona@gmail.com>
2022-12-08 15:27:28 +0100

    Filter

---
+++ src/components/searchbar/Searchbar.jsx
@@ -0,0 +1,14 @@
+const Searchbar = ({setQuery}) => {
+
+  const handleInputChange = event => {
+    setQuery(event.target.value)
+  }
+
+  return (
+    <form class="form-outline searchbar">
+      <input type="search" name="query" class="form-control" placeholder="Search" aria-label="Search" onChange={handleInputChange} />
+    </form>
+  )
+}
+
+export default Searchbar
--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -3,11 +3,16 @@ import axios from "axios";
 import './Products.css';
 import { BsFillHeartFill } from "react-icons/bs";
 import { BsBagPlusFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import Searchbar from "../../components/searchbar/Searchbar";
+import Dropdown from 'react-bootstrap/Dropdown';
+
 
 const Products = () => {
   const [products, setProducts] = useState([]);
-  const [hoveredOn, setHoveredOn] = useState(false);
+  const [hoveredOn, setHoveredOn] = useState("");
   const [isClicked, setIsClicked] = useState(false);
+  const [query, setQuery] = useState("")
 
   const getAllProducts = () => {
 
@@ -25,21 +30,45 @@ const Products = () => {
     setIsClicked(!isClicked)
   }
 
+  const filteredProducts = products.filter(product => {
+    return product.model.toLowerCase().includes(query.toLowerCase());
+  });
+
+
+  function handleSorting(sortingOption) {
+    sortingOption === "price_asc" && products.sort((a, b) => {return a.price - b.price})
+    sortingOption === "price_desc" && products.sort((a, b) => {return b.price - a.price})
+  }
+
   return (
     <div className="Products">
       <section className="products-header">
         <h1>Title</h1>
       </section>
+      <section className="search-section">
+        <Searchbar setQuery={setQuery}/>
+        <Link className="btn btn-outline-dark search-btn">Filter</Link>
+        <Dropdown>
+          <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
+            Sort
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={() => {handleSorting("price_asc")}}>Price: lowest</Dropdown.Item>
+            <Dropdown.Item onClick={() => {handleSorting("price_desc")}}>Price: highest</Dropdown.Item>
+            <Dropdown.Item onClick={() => {handleSorting("default")}}>Default</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </section>
       <section className="products-grid">
-        {products.map((product) => {
+        {filteredProducts.map((product) => {
           return (
-            <div className="product-card" key={product._id} onMouseEnter={setHoveredOn(true)} onMouseLeave={setHoveredOn(false)}>
+            <div className="product-card" key={product._id} onMouseEnter={()=> {setHoveredOn(product._id)}} onMouseLeave={()=> {setHoveredOn("")}}>
               <img id="product-card-img" src={product.image} alt={product.model} />
               <div>
-                <h5 className={hoveredOn && "card-title-dark"}>{product.model}</h5>
+                <h5 className={hoveredOn === product._id && "card-title-dark"}>{product.model}</h5>
                 <p id="card-price">{product.price} €</p>
               </div>
-              {hoveredOn &&
+              {hoveredOn === product._id &&
                 <div>
                   <BsFillHeartFill onClick={handleClick} style={{color: isClicked ? 'red' : 'black'}}/>
                   <BsBagPlusFill/>
